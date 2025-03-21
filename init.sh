1fe34d10f3f44e4e9dc4b53dc50c01d6f6695e5f
koki-nonaka <koki-nonaka@outloo.jp>
2025-03-21 19:49:11 +0900

    全体的に構成を見直し

--- init.sh
+++ init.sh
@@ -2,19 +2,83 @@
 
 set -Eeuo pipefail
 
-OP_USER="tech"
+TEMP=$(getopt -o u:t:h -l user:,timer:,help -- "${@}")
+eval set -- "{$TEMP}"
 
-function password_less_privilege_escalation() {
-  if [ ! -d "/etc/sudoers.d" ]; then
-      mkdir -p /etc/sudoers.d
+op_user=""
+timer="hourly"
+
+function usage() {
+    cat <<_EOF_
+Usage: $(basename $0) [OPTION]
+
+Description: "$(pwd)"  WSL2セットアップ用のシェルスクリプトです。
+
+Options:
+    -u --user  | required     : システム開発を行うユーザーを指定します。
+                                ユーザーは事前に作成済みである必要があります。
+    -t --timer | optionnal    : プロビジョニングを行う頻度を指定します。
+                                デフォルトでは1時間毎にプロビジョニングを行います。
+                                プロビジョニングはsystemd-timerを利用している為、以下の書式で指定可能です。
+                                - https://www.freedesktop.org/software/systemd/man/latest/systemd.time.html
+    -h --help  | optional     : ヘルプを表示します。
+_EOF_
+}
+
+function parameter_parsing() {
+  while true; do
+    case "${1}" in
+      -u|--user)
+        op_user=${2}
+        echo "user: $op_user"
+        shift 2
+        ;;
+      -t|--timer)
+        timer=${2}
+        echo "timer: $timer"
+        shift 2
+        ;;
+      -h|--help)
+        usage
+        exit 0
+        ;;
+      --)
+        shift
+        break
+        ;;
+      *)
+        echo "Unknown argument: ${1}"
+        usage
+        exit 9
+        ;;
+    esac
+  done
+
+  if [ -z "${op_user}" ]; then
+    echo "User is not specified."
+    exit 9
   fi
 
-  if [ ! -f "/etc/sudoers.d/${OP_USER}" ]; then
-      echo "${OP_USER} ALL=(ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/${OP_USER} > /dev/null
-      chmod 440 /etc/sudoers.d/${OP_USER}
+  if [ -z "${timer}" ]; then
+    echo "Timer is not specified."
+    exit 9
+  fi
+}
+
+function password_less_privilege_escalation() {
+  user=$1
+  sudoers_path="/etc/sudoers.d"
+
+  if [ ! -d "${sudoers_path}" ]; then
+      mkdir -p ${sudoers_path}
+  fi
+
+  if [ ! -f "${sudoers_path}/${user}" ]; then
+      echo "${user} ALL=(ALL) NOPASSWD: ALL" | tee ${sudoers_path}/${user} > /dev/null
+      chmod 440 ${sudoers_path}/${user}
   else
-      if ! grep -q "${OP_USER} ALL=(ALL) NOPASSWD: ALL" /etc/sudoers.d/${OP_USER}; then
-          echo "${OP_USER} ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers.d/${OP_USER} > /dev/null
+      if ! grep -q "${user} ALL=(ALL) NOPASSWD: ALL" ${sudoers_path}/${user}; then
+          echo "${user} ALL=(ALL) NOPASSWD: ALL" | tee -a ${sudoers_path}/${user} > /dev/null
       fi
   fi
 
@@ -22,7 +86,9 @@ function password_less_privilege_escalation() {
 
 function requirement_package() {
     apt-add-repository ppa:ansible/ansible -y
-    apt update && apt upgrade -y
+
+    apt update
+    apt upgrade -y
 
     apt install -y \
       ansible \
@@ -31,35 +97,12 @@ function requirement_package() {
     apt autoremove -y
 }
 
-function ansible_setup() {
-    ansible_logtotate_conf="/etc/logrotate.d/ansible-pull"
-    ansible_log_dir="/home/${OP_USER}/.logs"
-    ansible_pull_log_filename="ansible-pull.log"
-
-    if [ ! -d "${ansible_log_dir}" ]; then
-        echo "Creating directory: ${ansible_log_dir}"
-        mkdir -p "${ansible_log_dir}"
-        chmod -R 755 "${ansible_log_dir}"
-        chown -R ${OP_USER}:${OP_USER} "${ansible_log_dir}"
-        echo "Directory created and permissions set."
-    else
-        echo "Directory already exists: ${ansible_log_dir}"
-    fi
-
-    cat <<EOF > ${ansible_logtotate_conf}
-${ansible_log_dir}/${ansible_pull_log_filename} {
-    compress
-    missingok
-    rotate 365
-    daily
-    dateext
-    copytruncate
-}
-EOF
-
-    logrotate -d ${ansible_logtotate_conf}
-}
-
-password_less_privilege_escalation
+parameter_parsing "${@}"
+password_less_privilege_escalation "${op_user}"
 requirement_package
-ansible_setup
+
+home_dir="/home/${op_user}"
+sudo -u ${op_user} \
+  bash -c "cd ${home_dir} && /usr/bin/ansible-pull -U https://github.com/kukv/wsl-setup.git -i inventory.yaml playbook.yaml -e '{\"provisioning_schedule\":\"${timer}\"\}'"
+
+exit 0
